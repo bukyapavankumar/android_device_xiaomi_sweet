d476431fdeca1b01649fade299a78e58019c04cd
VladV1V <mahno444@gmail.com>
2021-05-04 00:03:00 +0300

    sweet: intial 404 bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/p404_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    p404_sweet-user \
+    p404_sweet-userdebug \
+    p404_sweet-eng
--- p404_sweet.mk
+++ p404_sweet.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some 404 common configuration.
+$(call inherit-product, vendor/404/configs/common.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := p404_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
